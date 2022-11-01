b4a1c9f6d789d2b08a440509e39ce4f76ba8f919
Brooke <brooke@gril.me>
2022-11-01 22:13:10 +0000

    Complete basic example

--- example/src/client/init.client.lua
+++ example/src/client/init.client.lua
@@ -5,12 +5,21 @@ local Packages = ReplicatedStorage:WaitForChild("Packages")
 local Roact = require(Packages:WaitForChild("Roact"))
 
 local e = Roact.createElement
+local useState = Roact.useState
 
 local function Button()
+    local clicks, setClicks = useState(0)
+    
     return e("TextButton", {
         AnchorPoint = Vector2.new(0.5, 0.5),
         Position = UDim2.fromScale(0.5, 0.5),
         Size = UDim2.fromOffset(240, 240),
+
+        Text = "Clicks: " .. clicks,
+
+        [Roact.Event.Activated] = function()
+            setClicks(clicks + 1)
+        end,
     })
 end
 
--- example/src/shared/Hello.lua
+++
@@ -1,3 +0,0 @@
-return function()
-	print("Hello, world!")
-end
